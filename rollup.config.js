a3538fe82bf25d0011671ce0dd5d8c1d857a3b94
Mihail Silantev <gmod2007@gmail.com>
2024-10-24 15:15:22 +0300

    refactor: rollup config refactor

--- rollup.config.js
+++ rollup.config.js
@@ -2,57 +2,57 @@ import babel from "@rollup/plugin-babel";
 import typescript from '@rollup/plugin-typescript';
 import external from "rollup-plugin-peer-deps-external";
 import commonjs from "@rollup/plugin-commonjs";
-import resolve from "@rollup/plugin-node-resolve";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 import postcss from "rollup-plugin-postcss";
 import url from "@rollup/plugin-url";
 
 import pkg from "./package.json";
 
 export default {
-  input: "src/index.tsx",
-  external: ["react"],
-  output: [
-    {
-      file: pkg.main,
-      format: "cjs",
-      sourcemap: true,
-      globals: {
-        react: "React",
-      },
-    },
-    {
-      file: pkg.module,
-      format: "es",
-      sourcemap: true,
-      globals: {
-        react: "React",
-      },
-    },
-    {
-      file: pkg.browser,
-      format: "umd",
-      name: "react-json-grid",
-      sourcemap: true,
-      globals: {
-        react: "React",
-      },
-    },
-  ],
-  plugins: [
-    external(),
-    typescript(),
-    postcss({
-      autoModules: false,
-      modules: {
-        generateScopedName: "[local]",
-      },
-    }),
-    url(),
-    babel({
-      babelHelpers: "bundled",
-      exclude: "node_modules/**",
-    }),
-    resolve(),
-    commonjs(),
-  ],
+    input: "src/index.tsx",
+    external: ["react"],
+    output: [
+        {
+            file: pkg.main,
+            format: "cjs",
+            sourcemap: true,
+            globals: {
+                react: "React",
+            },
+        },
+        {
+            file: pkg.module,
+            format: "es",
+            sourcemap: true,
+            globals: {
+                react: "React",
+            },
+        },
+        {
+            file: pkg.browser,
+            format: "umd",
+            name: "react-json-grid",
+            sourcemap: true,
+            globals: {
+                react: "React",
+            },
+        },
+    ],
+    plugins: [
+        external(),
+        typescript(),
+        postcss({
+            autoModules: false,
+            modules: {
+                generateScopedName: "[local]",
+            },
+        }),
+        url(),
+        babel({
+            babelHelpers: "bundled",
+            exclude: "node_modules/**",
+        }),
+        nodeResolve(),
+        commonjs(),
+    ],
 };
